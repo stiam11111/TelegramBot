493bf4be3f066e119908556c3d40c45ec3db2849
Aligan18 <alisherganiev18@gmail.com>
2023-10-07 19:50:43 +0600

    I fix all

--- apps/MyPyrogram/pyrogram_bot.py
+++ apps/MyPyrogram/pyrogram_bot.py
@@ -19,31 +19,60 @@ SOURCE_PUBLICS = [
 ]
 PHONE_NUMBER = os.getenv("PHONE_NUMBER")  # номер зарегистрованный в телеге
 
+media_group_id = 0
 
 class PyrogramBot:
     # Создать можно на my.telegram.org
-
     # создаем клиент телеграм
     app = Client("posts_collector", api_id=API_ID, api_hash=API_HASH,
                  phone_number=PHONE_NUMBER)
-
+    last_media_group_id = None
+    count = 1
     # обработчик нового сообщения
     # вызывается при появлении нового поста в одном из пабликов-доноров
+
+    # @app.on_message(filters.chat(SOURCE_PUBLICS))
+    # def new_channel_post(client, message):
+    #     # сохраняем пост в базу (функцию add_post_to_db определим потом)
+    #     global media_group_id
+    #     post_id = data_base.add_post_to_db(message)
+    #     # пересылаем пост в скрытый паблик
+    #     print(message)
+    #     if message.media_group_id:
+    #         client.copy_media_group(-1001813831972, message.chat.id, message.id)
+    #         if media_group_id != message.media_group_id:
+    #             media_group_id = message.media_group_id
+    #             myTeleBot.bot.send_message(-1001813831972, media_group_id)
+    #     else:
+    #         client.copy_message(-1001813831972, message.chat.id, message.id)
+    #         myTeleBot.bot.send_message(-1001813831972, '1')
+
     @app.on_message(filters.chat(SOURCE_PUBLICS))
     def new_channel_post(client, message):
-        # сохраняем пост в базу (функцию add_post_to_db определим потом)
-        post_id = data_base.add_post_to_db(message)
-        # пересылаем пост в скрытый паблик
-        client.copy_message(-1001813831972 , message.chat.id, message.id)
-        # потом для пересылки в публичный паблик админ должен отправить боту этот id
+        # Check if the message is part of a media group and the media group ID is different
+        if message.media_group_id and message.media_group_id != PyrogramBot.last_media_group_id:
+            # Save the new media_group_id
+            PyrogramBot.last_media_group_id = message.media_group_id
+            PyrogramBot.count = 1
+            # Process the media group (e.g., copy it to the destination chat)
+            client.copy_media_group(-1001813831972, message.chat.id, message.id)
+            myTeleBot.bot.send_message(-1001813831972, message.media_group_id)
+
+        else :
+            PyrogramBot.count = PyrogramBot.count + 1
+
+    # потом для пересылки в публичный паблик админ должен отправить боту этот id
 
     # обработчик нового сообщения из скрытого паблика
     # если админ пишет в паблик `132+` это значит переслать пост с id = 132 в публичный паблик
 
     @app.on_message(filters.chat(-1001813831972))
     def post_request(client, message):
-        print('New Post', message)
-        myTeleBot.bot.copy_message(myTeleBot.client_chat_id, message.chat.id, message.id)
+        print()
+        count = PyrogramBot.count
+        while count > 0:
+            myTeleBot.bot.copy_message(myTeleBot.client_chat_id, message.chat.id, message.id - count)
+            count = count -1
 
 
 
--- apps/MyTelebot/bot.py
+++ apps/MyTelebot/bot.py
@@ -19,4 +19,6 @@ class TeleBot:
 
 
 
+
+
 myTeleBot = TeleBot()
