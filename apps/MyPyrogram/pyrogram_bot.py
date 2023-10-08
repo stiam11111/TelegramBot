da49a64638a15bd794fe9eb1ad8b275100e50075
Aligan18 <alisherganiev18@gmail.com>
2023-10-08 15:27:07 +0600

    I added method post_request

--- apps/MyPyrogram/pyrogram_bot.py
+++ apps/MyPyrogram/pyrogram_bot.py
@@ -28,43 +28,24 @@ class PyrogramBot:
                  phone_number=PHONE_NUMBER)
     last_media_group_id = None
     count = 1
-    # обработчик нового сообщения
-    # вызывается при появлении нового поста в одном из пабликов-доноров
 
-    # @app.on_message(filters.chat(SOURCE_PUBLICS))
-    # def new_channel_post(client, message):
-    #     # сохраняем пост в базу (функцию add_post_to_db определим потом)
-    #     global media_group_id
-    #     post_id = data_base.add_post_to_db(message)
-    #     # пересылаем пост в скрытый паблик
-    #     print(message)
-    #     if message.media_group_id:
-    #         client.copy_media_group(-1001813831972, message.chat.id, message.id)
-    #         if media_group_id != message.media_group_id:
-    #             media_group_id = message.media_group_id
-    #             myTeleBot.bot.send_message(-1001813831972, media_group_id)
-    #     else:
-    #         client.copy_message(-1001813831972, message.chat.id, message.id)
-    #         myTeleBot.bot.send_message(-1001813831972, '1')
 
     @app.on_message(filters.chat(SOURCE_PUBLICS))
     def new_channel_post(client, message):
         # Check if the message is part of a media group and the media group ID is different
-        if message.media_group_id and message.media_group_id != PyrogramBot.last_media_group_id:
+        if message.media_group_id :
             # Save the new media_group_id
-            PyrogramBot.last_media_group_id = message.media_group_id
-            PyrogramBot.count = 1
-            # Process the media group (e.g., copy it to the destination chat)
-            client.copy_media_group(-1001813831972, message.chat.id, message.id)
-            myTeleBot.bot.send_message(-1001813831972, message.media_group_id)
-
-        else :
-            PyrogramBot.count = PyrogramBot.count + 1
-
-    # потом для пересылки в публичный паблик админ должен отправить боту этот id
-
-    # обработчик нового сообщения из скрытого паблика
-    # если админ пишет в паблик `132+` это значит переслать пост с id = 132 в публичный паблик
+            if  message.media_group_id != PyrogramBot.last_media_group_id:
+                PyrogramBot.last_media_group_id = message.media_group_id
+                PyrogramBot.count = 1
+                # Process the media group (e.g., copy it to the destination chat)
+                client.copy_media_group(-1001813831972, message.chat.id, message.id)
+                myTeleBot.bot.send_message(-1001813831972, message.media_group_id)
+            else :
+                PyrogramBot.count = PyrogramBot.count + 1
+        else:
+            client.copy_message(-1001813831972, message.chat.id, message.id)
+            myTeleBot.bot.send_message(-1001813831972, '1')
 
     @app.on_message(filters.chat(-1001813831972))
     def post_request(client, message):
@@ -78,29 +59,3 @@ class PyrogramBot:
 
 pyrogram_bot = PyrogramBot()
 
-
-# получаем из базы пост по этому id
-        # post = data_base.db.get(post_id)
-        # print(message)
-        # if post is None:
-        #     # если нет в базе пишем в скрытый паблик ошибку
-        #     client.send_message(PRIVATE_PUBLIC,
-        #                         '`ERROR NO POST ID IN DB`')
-        #     # и выходим
-        #     return
-        #
-        # try:
-        #     # по данным из базы, получаем pyrogram обьект сообщения
-        #     #
-        #     # message_id = (post['message_id'] * -1) - 1000000000000
-        #
-        #     msg = client.get_messages(post['username'], post['message_id'])
-        #     # пересылаем его в паблик
-        #     # as_copy=True значит, что мы не будем отображать паблик донор, будто это наш пост XD
-        #
-        #     msg.copy(PUBLIC_PUBLIC)
-        #     # отправляем сообщение в скрытый паблик о успехе
-        #     client.send_message(PRIVATE_PUBLIC, f'`SUCCESS REPOST!`')
-        # except Exception as e:
-        #     # если произойдет какая-то ошибка в 3 строчках выше - сообщим админу
-        #     client.send_message(PRIVATE_PUBLIC, f'`ERROR {e}`')
